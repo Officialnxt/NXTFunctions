0305b0adfdccacfcb6b0f0cf5844fe4346d3de73
Officialnxt <esmith233@textfree.us>
2013-09-26 18:45:42 -0400

    Update math.php

--- math.php
+++ math.php
@@ -1,6 +1,6 @@
 <?php
 
-function maths($mathString) {
+function calculate($mathString) {
 	if($mathString == "pi"){
 		return pi();
 	}
@@ -42,6 +42,79 @@ function metersToYards($meters, $echo = true){
 	$m = $meters;
 	$Yards = $m*1.09361;
 	$data = $Yards;
+	if($echo == true)
+	{
+		echo $data;
+	} else {
+		return $data;
+	}
+}
+
+
+function metersToCentimeters($meters, $echo = true){
+	$m = $meters;
+	$Centi = $m*100;
+	$data = $Centi;
+	if($echo == true)
+	{
+		echo $data;
+	} else {
+		return $data;
+	}
+}
+
+function metersToMiliimeters($meters, $echo = true){
+	$m = $meters;
+	$Mili = $m*1000;
+	$data = $Mili;
+	if($echo == true)
+	{
+		echo $data;
+	} else {
+		return $data;
+	}
+}
+
+function metersToDecimeters($meters, $echo = true){
+	$m = $meters;
+	$Deci = $m*10;
+	$data = $Deci;
+	if($echo == true)
+	{
+		echo $data;
+	} else {
+		return $data;
+	}
+}
+
+function metersToDekameters($meters, $echo = true){
+	$m = $meters;
+	$Deka = $m*0.1;
+	$data = $Deka;
+	if($echo == true)
+	{
+		echo $data;
+	} else {
+		return $data;
+	}
+}
+
+function metersToHectometers($meters, $echo = true){
+	$m = $meters;
+	$Hecto = $m*0.01;
+	$data = $Hecto;
+	if($echo == true)
+	{
+		echo $data;
+	} else {
+		return $data;
+	}
+}
+
+function metersToKilometers($meters, $echo = true){
+	$m = $meters;
+	$Kilo = $m*0.1;
+	$data = $Kilo;
 	if($echo == true)
 	{
 		echo $data;
